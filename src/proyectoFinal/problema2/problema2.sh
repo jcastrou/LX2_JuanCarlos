1540c44fdb5296bbf610c1f8ac880a36dfdb24e2
Juan Carlos Castro <juanccastrou@gmail.com>
2016-05-27 16:03:49 -0600

    History Actualizado y problema #2 avanzado

--- src/proyectoFinal/problema2/problema2.sh
+++ src/proyectoFinal/problema2/problema2.sh
@@ -2,6 +2,9 @@
 	Data=/home/cujc/Administrador_de_Servidores/LX2_JuanCarlos/src/proyectoFinal/problema2
 mkdir $Data/datos_csv
 OUT_DATA=$Data/datos_csv
+GRAF_DATA=$Data/datos_graf
+mkdir $GRAF_DATA
+
 
 m=0
 
@@ -10,5 +13,14 @@ for j in `find $DATA -name '*.xls'`
 	echo "Procesando  Archivo $j"
 	xls2csv $j > $OUT_DATA/datos-$m.csv
 	let m=m+1
-	done 2>error1.log
+	done 2>/home/cujc/Administrador_de_Servidores/LX2_JuanCarlos/error/ErrorConversionDeArchivos.log
  
+
+m=0
+for p in `find $OUT_DATA -name "*.csv"`
+do
+	echo "Procesando datos del archivo $p para generar el grafico correspondiente al AGUA desde el mes de ENERO a JUNIO"
+	cat $p | awk -F "\",\"" '{print $2}' | sed '1,$ s/"//g' | sed '2 s/AGUA/#AGUA/g' > $GRAF_DATA/grafico-$m.dat
+	let m=m+1
+	done 2>/home/cujc/Administrador_de_Servidores/LX2_JuanCarlos/error/ErrorGrafica.log
+
